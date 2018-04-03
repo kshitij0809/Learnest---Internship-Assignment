c75a90a4e8dceae208885eb5e988ee1238065f22
Kshitij Pratap Singh Bhadauria <kshitijpratap0809@gmail.com>
2018-04-03 16:48:07 +0530

    Three.jsx file added dummy data and maintain template

--- src/components/Three.jsx
+++ src/components/Three.jsx
@@ -21,7 +21,27 @@ import './Three.css';
     }
     
   }
- 
+  componentDidMount () {
+   
+
+
+    this.setState({ mappedMainEvents: [{
+    title:'KSHITIJ',
+    tagline:'IIT ROORKEE STUDENT',
+    short_description:"Events are a great way to create awareness, amass knowledge and generate interest in topics while having some fun, all through the competitive spirit. The Summit brings to you a wide ",  
+    },
+    {
+    title:'KSHITIJ',
+    tagline:'IIT ROORKEE STUDENT',
+    short_description:"Events are a great way to create awareness, amass knowledge and generate interest in topics while having some fun, all through the competitive spirit. The Summit brings to you a wide ",  
+    },
+    {
+    title:'KSHITIJ',
+    tagline:'IIT ROORKEE STUDENT',
+    short_description:"Events are a great way to create awareness, amass knowledge and generate interest in topics while having some fun, all through the competitive spirit. The Summit brings to you a wide ",  
+    },
+    ]});
+  }
  
   render () {
     return (
@@ -34,13 +54,53 @@ import './Three.css';
         </Col>
         </Row>
         </Grid>
-       
+        <Grid>
+          <Row>
+        {this.state.mappedMainEvents.map((event, key) => (
+          <div key={key}>
+            
+              <MainEventTemp
+                name={event.title}
+                short={event.tagline}
+                long={event.short_description}
+                
+                            />
+          </div>
+                ))}
+          </Row>
+         
+          
+        </Grid>
       </div>
     )
   }
 }
 
+class MainEventTemp extends Component {
+  constructor (props) {
+    super(props)
+  }
 
+  render () {
+    var compname = this.props.name;
+    
+    return (
+
+        
+  
+           <Col md={4} sm={4} xs={4} className="card">
+              <div className='main-event'>
+               <div>{this.props.name}</div>
+               <div>{this.props.short}</div>
+               <div>{this.props.long}</div>
+              </div>
+            </Col>      
+        
+      
+    )
+
+  }
+}
 
 export default ButThree;
 
