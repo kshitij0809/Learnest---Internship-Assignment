3486a05ea50047d5ae21ef299b354aefa3ade282
Kshitij Pratap Singh Bhadauria <kshitijpratap0809@gmail.com>
2018-04-03 14:03:49 +0530

    initalizing the basci app for frontend in reactjs

--- README.md
+++
@@ -1,2 +0,0 @@
-# Learnest---Internship-Assignment
-Assignment give to me for recruitnig for internship. the task is for both Front-end development as well as for the backend development.
---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Udemy from './components/Home';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={Udemy} />
+         
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
