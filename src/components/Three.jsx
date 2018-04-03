4339988066416a460c8b233ba702e5c11bba93fb
Kshitij Pratap Singh Bhadauria <kshitijpratap0809@gmail.com>
2018-04-03 16:40:20 +0530

    Three.jsx file added for other component

---
+++ src/components/Three.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import { Jumbotron, Grid, Row, Col, Image, Button ,ButtonToolbar} from 'react-bootstrap';
+
+
+import {NavLink} from 'react-router-dom';
+
+import {iitrLogo}from '../logo.svg';
+
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Glyphicon } from 'react-bootstrap'
+import './Three.css';
+
+
+
+
+ class ButThree extends Component {
+  constructor (props, context) {
+    super(props, context)
+    this.state = {
+      mappedMainEvents: []
+    }
+    
+  }
+ 
+ 
+  render () {
+    return (
+      <div>
+        <Grid>
+        <Row>
+          <Col md={12} sm={12} lg={12}  className='heading-landing'>
+                        highlighted events
+              
+        </Col>
+        </Row>
+        </Grid>
+       
+      </div>
+    )
+  }
+}
+
+
+
+export default ButThree;
+
+
