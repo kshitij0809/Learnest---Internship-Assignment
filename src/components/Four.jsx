16dbba2d9c4f10cec23b6228b66a309b5aaf9b61
Kshitij Pratap Singh Bhadauria <kshitijpratap0809@gmail.com>
2018-04-04 18:50:40 +0530

    Four.Jsx file completed with full dummy data from api call

--- src/components/Four.jsx
+++ src/components/Four.jsx
@@ -8,6 +8,7 @@ import {NavLink} from 'react-router-dom';
 import {iitrLogo}from '../logo.svg';
 
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Glyphicon } from 'react-bootstrap'
+import './Four.css';
 
 
 
@@ -20,7 +21,57 @@ import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Glyphicon } from 'react-bo
     }
     
   }
- 
+  componentDidMount () {
+   
+
+
+    this.setState({ mappedMainEvents: [{
+    title:'KSHITIJ',
+    tagline:'IIT ROORKEE STUDENT',
+    short_description:"Events are a great way to create awareness, amass knowledge and generate interest in topics while having some fun, all through the competitive spirit. The Summit brings to you a wide ",  
+    },
+    {
+    title:'KSHITIJ',
+    tagline:'IIT ROORKEE STUDENT',
+    short_description:"Events are a great way to create awareness, amass knowledge and generate interest in topics while having some fun, all through the competitive spirit. The Summit brings to you a wide ",  
+    },
+    {
+    title:'KSHITIJ',
+    tagline:'IIT ROORKEE STUDENT',
+    short_description:"Events are a great way to create awareness, amass knowledge and generate interest in topics while having some fun, all through the competitive spirit. The Summit brings to you a wide ",  
+    },
+    {
+    title:'KSHITIJ',
+    tagline:'IIT ROORKEE STUDENT',
+    short_description:"Events are a great way to create awareness, amass knowledge and generate interest in topics while having some fun, all through the competitive spirit. The Summit brings to you a wide ",  
+    },
+    {
+    title:'KSHITIJ',
+    tagline:'IIT ROORKEE STUDENT',
+    short_description:"Events are a great way to create awareness, amass knowledge and generate interest in topics while having some fun, all through the competitive spirit. The Summit brings to you a wide ",  
+    },
+    {
+    title:'KSHITIJ',
+    tagline:'IIT ROORKEE STUDENT',
+    short_description:"Events are a great way to create awareness, amass knowledge and generate interest in topics while having some fun, all through the competitive spirit. The Summit brings to you a wide ",  
+    },
+    {
+    title:'KSHITIJ',
+    tagline:'IIT ROORKEE STUDENT',
+    short_description:"Events are a great way to create awareness, amass knowledge and generate interest in topics while having some fun, all through the competitive spirit. The Summit brings to you a wide ",  
+    },
+    {
+    title:'KSHITIJ',
+    tagline:'IIT ROORKEE STUDENT',
+    short_description:"Events are a great way to create awareness, amass knowledge and generate interest in topics while having some fun, all through the competitive spirit. The Summit brings to you a wide ",  
+    },
+    {
+    title:'KSHITIJ',
+    tagline:'IIT ROORKEE STUDENT',
+    short_description:"Events are a great way to create awareness, amass knowledge and generate interest in topics while having some fun, all through the competitive spirit. The Summit brings to you a wide ",  
+    },
+    ]});
+  }
  
   render () {
     return (
@@ -33,14 +84,49 @@ import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Glyphicon } from 'react-bo
         </Col>
         </Row>
         </Grid>
-       
+        <Grid className='main-events-grid'>
+        <Row>
+        {this.state.mappedMainEvents.map((event, key) => (
+          <div key={key}>
+            
+              <MainEventTemp
+                name={event.title}
+                short={event.tagline}
+                long={event.short_description}
+                
+                            />
+            
+          </div>
+                ))}
+        </Row>
+        </Grid>
       </div>
     )
   }
 }
 
+class MainEventTemp extends Component {
+  constructor (props) {
+    super(props)
+  }
 
+  render () {
+    var compname = this.props.name;
+    
+    return (
 
+        <Col md={4} sm={4} xs={4} className="card">
+              <div className='main-event'>
+               <div>{this.props.name}</div>
+               <div>{this.props.short}</div>
+               <div>{this.props.long}</div>
+              </div>
+            </Col>      
+      
+    )
+
+  }
+}
 
 export default ButFour;
 
